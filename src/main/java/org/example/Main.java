614de6f07499c1ca1b92f234669354852762cff9
ekremkeser <ekrem.keser@etiqa.com.tr>
2024-09-13 14:34:26 +0300

    İlk 2 görev yapıldı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,36 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(656));
+        System.out.println(isPerfectNumber(32));
     }
-}
+
+    public static boolean isPalindrome(int n) {
+        int reverseN = 0;
+        int originalN = n;
+        while (n != 0) {
+            reverseN = reverseN * 10 + n % 10;
+            n /= 10;
+        }
+        if (originalN == reverseN) {
+            return true;
+        }
+        else {return false;}
+    }
+    public static boolean isPerfectNumber(int x) {
+        if (x <= 0) {
+            return false;
+        }
+        int sum = 1;
+        for (int i = 2; i < x; i++) {
+            if (x % i == 0) {
+                sum += i;
+            }
+
+        }if (sum == x) {
+            return true;
+        }
+        else {return false;}
+        }
+    }
+
